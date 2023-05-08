e68230353bdcd1c3b77daf7f86300dc1f02e66f6
Kwizera Claude <71407602+kclaudeeager@users.noreply.github.com>
2023-05-08 09:40:34 +0200

    added auto updater

--- app/src/main/java/com/itec/yussarent/ui/views/MainActivity.kt
+++ app/src/main/java/com/itec/yussarent/ui/views/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.itec.yussarent.ui.views
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
@@ -10,24 +12,43 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.InstallState
+import com.google.android.play.core.install.InstallStateUpdatedListener
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.InstallStatus
+import com.google.android.play.core.install.model.UpdateAvailability
+import com.google.android.play.core.ktx.isFlexibleUpdateAllowed
+import com.google.android.play.core.ktx.isImmediateUpdateAllowed
 import com.itec.yussarent.data.models.Screen
 import com.itec.yussarent.ui.theme.RentTheme
 import com.itec.yussarent.ui.views.components.LoginScreen
 import com.itec.yussarent.ui.views.components.MainScreen
 import com.itec.yussarent.ui.views.components.SplashScreen
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.seconds
 
 @AndroidEntryPoint
 class MainActivity:  ComponentActivity() {
 
+    private lateinit var appUpdateManager: AppUpdateManager
+    private val updateType  = AppUpdateType.FLEXIBLE
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+       appUpdateManager= AppUpdateManagerFactory.create(applicationContext)
+        if (updateType == AppUpdateType.FLEXIBLE){
+            appUpdateManager.registerListener(installStateUpdateListener)
+        }
+        checkForUpdates()
         setContent {
             RentTheme {
                 val navController = rememberNavController()
@@ -81,6 +102,75 @@ class MainActivity:  ComponentActivity() {
                     }
                 }
             }
+        }
+    }
+
+    private val installStateUpdateListener = InstallStateUpdatedListener{state->
+        if (state.installStatus() == InstallStatus.DOWNLOADED){
+        Toast.makeText(
+            applicationContext,
+            "Download successful. restarting app in 5 sec",
+            Toast.LENGTH_LONG
+        ).show()
+            lifecycleScope.launch {
+                delay(5.seconds)
+                appUpdateManager.completeUpdate()
+            }
+
+        }
+    }
+    private fun checkForUpdates(){
+        appUpdateManager.appUpdateInfo.addOnSuccessListener {info->
+            val isUpdateAvailable= info.updateAvailability()== UpdateAvailability.UPDATE_AVAILABLE
+            val isUpdateAllowed = when(updateType){
+                AppUpdateType.FLEXIBLE -> info.isFlexibleUpdateAllowed
+                AppUpdateType.IMMEDIATE -> info.isImmediateUpdateAllowed
+                else -> false
+            }
+            if (isUpdateAvailable && isUpdateAllowed){
+                appUpdateManager.startUpdateFlowForResult(
+                    info,
+                    updateType,
+                    this,
+                    100
+                )
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (updateType == AppUpdateType.IMMEDIATE) {
+            appUpdateManager.appUpdateInfo.addOnSuccessListener { info ->
+                if (info.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
+                    appUpdateManager.startUpdateFlowForResult(
+                        info,
+                        updateType,
+                        this,
+                        100
+                    )
+                }
+            }
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==100){
+            if(resultCode !=RESULT_OK){
+                Toast.makeText(
+                    applicationContext,
+                    "Something went wrong while updating",
+                    Toast.LENGTH_LONG
+                ).show()
+                println("Something went wrong while updating..")
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if(updateType == AppUpdateType.FLEXIBLE){
+            appUpdateManager.unregisterListener(installStateUpdateListener)
         }
     }
 }
